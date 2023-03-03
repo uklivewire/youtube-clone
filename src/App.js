ba4afb50d4cc480246fdc9a6edac965093b41782
uklivewire <rickybryans0@gmail.com>
2023-03-03 21:07:58 +0000

    first upload of files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App;
