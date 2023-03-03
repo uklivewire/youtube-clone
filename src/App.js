c2f97de5d3b5185a0760599d07c877617f1a22a1
uklivewire <rickybryans0@gmail.com>
2023-03-03 22:53:10 +0000

    routes added to app.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,17 @@ import { Box } from "@mui/material";
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+        <Box sx={{ backgroundColor: '#000' }}>
+            <Navbar />
+            <Routes>
+                <Route path='/' exact element={<Feed />} />
+                <Route path='/video/:id' element={<VideoDetail />} />
+                <Route path='/channel/:id' element={<ChannelDetail />} />
+                <Route path='/search/:searchTerm' element={<SearchFeed />} />
+            </Routes>
+        </Box>
+    </BrowserRouter>
   )
 }
 
