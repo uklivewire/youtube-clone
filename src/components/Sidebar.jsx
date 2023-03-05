f84272b5c027c81cab5cad0e62fb6271649ac8e8
uklivewire <rickybryans0@gmail.com>
2023-03-05 23:52:17 +0000

    added fetchFormAPI.js, videos.jsx and .env files

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,8 @@
 import { Stack } from '@mui/material';
 import { categories } from '../utils/constants';
 
-const selectedCategory = 'New';
 
-const Sidebar = () => (
+const Sidebar = ({selectedCategory, setselectedCategory}) => (
 
     <Stack direction="row" sx={{ overflowY: "auto", height: {sx: 'auto', md: '95%' }, flexDirection: { md: 'column'},}} >
         {categories.map((category) => (
